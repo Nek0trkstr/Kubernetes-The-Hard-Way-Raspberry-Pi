5a34b90102bd025762d641b24d5d0b1c195d4a18
Michael Shebeko <michael.shebeko@hcl.com>
2019-12-14 21:06:57 +0200

    Add kubeconfigs generation

---
+++ kubeconfigs/generate-kubeproxy-kubeconfig.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Generate kube-proxy kubeconfig
+
+KUBERNETES_ADDRESS=192.168.1.200
+
+{
+  kubectl config set-cluster kubernetes-the-hard-way \
+    --certificate-authority="../ca/ca.pem" \
+    --embed-certs=true \
+    --server=https://${KUBERNETES_ADDRESS}:6443 \
+    --kubeconfig=kube-proxy.kubeconfig
+
+  kubectl config set-credentials system:kube-proxy \
+    --client-certificate="../ca/kube-proxy.pem" \
+    --client-key="../ca/kube-proxy-key.pem" \
+    --embed-certs=true \
+    --kubeconfig=kube-proxy.kubeconfig
+
+  kubectl config set-context default \
+    --cluster=kubernetes-the-hard-way \
+    --user=system:kube-proxy \
+    --kubeconfig=kube-proxy.kubeconfig
+
+  kubectl config use-context default --kubeconfig=kube-proxy.kubeconfig
+}
