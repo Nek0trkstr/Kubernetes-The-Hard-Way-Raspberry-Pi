16874e34a3334413c695c060312e1855b3e99200
Michael Shebeko <michael.shebeko@hcl.com>
2019-12-22 20:12:17 +0200

    Add Kernel patch for weave net

--- networking/network_config.sh
+++ networking/network_config.sh
@@ -3,6 +3,7 @@
 
 sudo sysctl net.ipv4.conf.all.forwarding=1
 echo "net.ipv4.conf.all.forwarding=1" | sudo tee -a /etc/sysctl.conf
-sudo rpi-update
+sudo rpi-update # Needed for weave net
 
+# from client machine
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')&env.IPALLOC_RANGE=10.200.0.0/16"
